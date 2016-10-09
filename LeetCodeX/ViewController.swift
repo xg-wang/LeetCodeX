fd7f46ed45f87fe98e55181f354418f9fb71e682
Xingan Wang <wangxgwxg@gmail.com>
2016-10-09 09:46:11 -0700

    Initial Commit

---
+++ LeetCodeX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeetCodeX
+//
+//  Created by Xingan Wang on 10/9/16.
+//  Copyright © 2016 Xingan Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
